fa86e81c44ea053c8f5411450b4b1883994c8511
aggsdp <aggsdpz@gmail.com>
2022-09-06 17:53:18 -0300

    atualizacao do exercicio 3 finalizado com a versão de listagem de forma decrescente

--- poo-udesc/AP1-Ex2Decrescente/src/atividadepratica1/Main.java
+++ poo-udesc/AP1-Ex2Decrescente/src/atividadepratica1/Main.java
@@ -5,15 +5,31 @@ public class Main {
         Scanner leitor = new Scanner(System.in);
         String[] nomePessoa = new String[5];
         int[] idadePessoa = new int[5];
-        int ordemDescrescente = 150;
-        int j = 0;
+        //int ordemDescrescente = 150;
+        //int j = 0;
+        int[] auxIdade = new int[5];
+        String[] auxNome = new String[5];
 
-        for(int i=0; i<3; i++){
+        for(int i=0; i<5; i++){
             nomePessoa[i] = leitor.next();
             idadePessoa[i] = leitor.nextInt();
         }
-        
-        for(int i=0;i<3;i++){
+
+        //ordenacao do vetor
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+                if(idadePessoa[j]>idadePessoa[i]){
+                    auxIdade[i] = idadePessoa[j];
+                    idadePessoa[j] = idadePessoa[i];
+                    idadePessoa[i] = auxIdade[i];
+                    auxNome[i] = nomePessoa[j];
+                    nomePessoa[j] = nomePessoa[i];
+                    nomePessoa[i] = auxNome[i];
+                }
+            }
+        }
+
+        for(int i=0;i<5;i++){
             System.out.println(nomePessoa[i]);
             System.out.println(idadePessoa[i]);
         }
