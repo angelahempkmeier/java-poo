840d956a2e3ef52c2626f7818890ee878200edb9
aggsdp <aggsdpz@gmail.com>
2022-09-07 14:14:43 -0300

    inicio da resolucao de exercicio sobre uma pessoa ser maior idade, verificacao se tudo está correndo certo com a pasta

---
+++ resolucao-exercicios/pessoaMaiorIdade/src/exercicioidade/Main.java
@@ -0,0 +1,6 @@
+package exercicioidade;
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ resolucao-exercicios/pessoaMaiorIdade/src/exercicioidade/Pessoa.java
@@ -0,0 +1,15 @@
+package exercicioidade;
+
+public class Pessoa {
+    String nome;
+    String sexo;
+    int idade;
+
+    public void maiorDeIdade(int idade){
+        if(idade >= 18){
+            System.out.println("Maior de idade.");
+        }else{
+            System.out.println("Menor de idade.");
+        }
+    }
+}
