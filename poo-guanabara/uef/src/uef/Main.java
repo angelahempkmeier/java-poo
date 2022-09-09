f555f938b1f36650736874b4acae174d199da243
aggsdp <aggsdpz@gmail.com>
2022-09-08 23:45:37 -0300

    finalizacao exercicio lutador

--- poo-guanabara/uef/src/uef/Main.java
+++ poo-guanabara/uef/src/uef/Main.java
@@ -11,9 +11,14 @@ public class Main {
         l[3] = new Lutador("Dead Code", "Austália", 28, 1.93f, 81.6f, 13, 0, 2);
         l[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
         l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
-        l[5].empatarLuta();
-        l[5].status();
+        //l[5].empatarLuta();
+        //l[5].status();
 
+        Luta uec01 = new Luta();
+        uec01.marcarLuta(l[1], l[1]);
+        uec01.lutar();
+        l[0].status();
+        l[1].status();
 
 
 
