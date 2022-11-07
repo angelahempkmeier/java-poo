39c9ca2f6ec8011a3910a81d4f8dfa328449397f
aggsdp <aggsdpz@gmail.com>
2022-11-07 10:54:08 -0300

    alteracoes

--- poo-udesc/Atividade7/a97-intcom/src/OperacoesBasicas.java
+++
@@ -1,4 +0,0 @@
-public interface OperacoesBasicas <T> {
-    public T somar(T operador1, T operador2);
-    public T subtrair(T operador1, T operador2);
-}
--- poo-udesc/Lista3/l3-ex1/src/Cachorro.java
+++
@@ -1,7 +0,0 @@
-public class Cachorro extends Animal{
-
-    @Override
-    public String emitirSom() {
-        return "Au au au";
-    }
-}
